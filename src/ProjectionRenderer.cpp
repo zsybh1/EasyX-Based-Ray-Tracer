fab2852f4ac75deec497103cf10f436c39b6ca11
zsybh1 <zsybh1@163.com>
2020-06-13 20:31:49 +0800

    Update

--- src/ProjectionRenderer.cpp
+++ src/ProjectionRenderer.cpp
@@ -19,7 +19,7 @@ int main() {
 	setaspectratio(1, -1);
 
 	// 视点设定
-	float dist = 480.0 * 3.0;
+	float dist = 4.0f;
 	pos e(0.0, 0.0, -dist);
 	vec3 v(0.0, 1.0, 0.0);
 	vec3 u(1.0, 0.0, 0.0);
@@ -32,67 +32,24 @@ int main() {
 	pos lightList[] = { light1, light2 };
 
 	// 物体设定
-	unsigned int objectNum = 5;
-	colored_sphere s1(pos(0.0, 200.0, 200.0), 50.0, color(BGR(RED)));
-	colored_sphere s2(pos(0.0, 50.0, 200.0), 100.0, color(255, 128, 64));
-	colored_sphere s3(pos(0.0, -250.0, 200.0), 200.0, color(BGR(GREEN)));
-	colored_triangle t1(pos(-1000.0, -HEIGHT / 2, 0.0), pos(1000.0, -HEIGHT / 2, 0.00), pos(4000.0, -HEIGHT / 2, 4000.0), color(BGR(BLUE)));
-	colored_triangle t2(pos(-1000.0, -HEIGHT / 2, 0.0), pos(-4000.0, -HEIGHT / 2, 4000.00), pos(4000.0, -HEIGHT / 2, 4000.0), color(BGR(BLUE)));
-	surface *objectList[] = { &s1, &s2, &s3, &t1, &t2};
+	unsigned int objectNum = 1;
+	triangle t1(pos(1.0, 1.0, 1.0), pos(2.0, 0.0, 1.0), pos(2.0, 1.0, 1.0));
+	triangle *objectList[] = {&t1};
 	
 	//============================
-	//== 遍历像素进行渲染
+	//== 遍历物体进行渲染
 	//============================
 	
-	for (int j = HEIGHT / 2; j > -HEIGHT / 2; --j) {
-		for (int i = -WIDTH / 2; i < WIDTH / 2; ++i) {
-			// 生成光线
-			vec3 d = -dist * w + (float)i * u + (float)j * v;
-			ray l(e, d);
-
-			// 初始化深度值
-			float t0 = 10000.0;
-			float t;
-
-			// 对每个物体求交
-			for (unsigned int k = 0; k < objectNum; ++k) {
-				// 通过深度测试则绘制像素点
-				if (objectList[k]->intersect_ray(l, t) && t < t0) {
-					if (typeid(*objectList[k]) == typeid(colored_sphere) || typeid(*objectList[k]) == typeid(colored_triangle)) {
-						colored_surface &ob = *dynamic_cast<colored_surface *>(objectList[k]);
-						color result;
-						pos p = l.e + t * l.d;
-						vec3 n = objectList[k]->normal(p - d.normalize() * 0.01);
-						vec3 viewDir = (-l.d).normalize();
-						// 环境光
-						result += 0.2f * ob.rgb;
-						float power = 100;
-
-						for (unsigned int li = 0; li < lightNum ; ++li) {
-							vec3 lightLine = lightList[li] - p;
-							vec3 lightDir = lightLine.normalize();
-							ray lightRay(p, lightLine);
-							vec3 halfDir = (viewDir + lightDir).normalize();
-							float dis;
-							bool flag = true;
-							for (unsigned int ki = 0; ki < objectNum; ++ki) {
-								if (ki == k) continue;
-								if (objectList[ki]->intersect_ray(lightRay, dis) && dis <= 1) {
-									flag = false;
-									break;
-								}
-							}
-							if (flag)
-								result += 0.5f * max(0.0f, dot(n, lightDir)) * ob.rgb + 1.0f * pow(max(0.0f, dot(n, halfDir)), power) * ob.rgb;
-						}
-
-						putpixel(i, j, result.getBGR());
-
-						// 更新深度值
-						t0 = t;
-					}
-				}
-			}
+	for (int i = 0; i < objectNum; ++i) {
+		triangle &tri = *objectList[i];
+		pos *r[3] = { &tri.A,&tri.B,&tri.C};
+		for (int j = 0; j < 3; ++j) {
+			float t = -r[j]->z / e.z;
+			float x = e.x + t * r[j]->x;
+			float y = e.y + t * r[j]->y;
+			int x0 = x * 200;
+			int y0 = y * 200;
+			putpixel(x0, y0, WHITE);
 		}
 	}
 	saveimage(L"test.png");
