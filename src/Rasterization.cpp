ab102590b33c01e7b12d938e1e95be1574a73c34
zsybh1 <zsybh1@163.com>
2020-06-14 16:02:43 +0800

    Update Rasterization.cpp

--- src/Rasterization.cpp
+++ src/Rasterization.cpp
@@ -23,16 +23,7 @@ int main() {
 	// 视点设定
 	float dist = 3.0f;
 	pos e(0.0, 0.0, -dist);
-	vec3 v(0.0, 1.0, 0.0);
-	vec3 u(1.0, 0.0, 0.0);
-	vec3 w(0.0, 0.0, -1.0);
     
-	//灯光设定
-	unsigned int lightNum = 2;
-	pos light1(0.0, 400.0, 50.0);
-	pos light2(300.0, 400.0, 0.0);
-	pos lightList[] = { light1, light2 };
-
 	// 物体设定
 	unsigned int objectNum = 1;
 	tricolor_triangle t1(pos(-1.0, -1.0, 1.0), pos(2.5, -2.0, 1.0), pos(2.0, 2.0, 1.0), BGR(RED), BGR(GREEN), BGR(BLUE));
@@ -59,25 +50,31 @@ void draw_tricolor_triangle(const tricolor_triangle &tri, const pos &e) {
 	const pos *r[3] = { &tri.A,&tri.B,&tri.C };
 	pos p[3];
 	for (int j = 0; j < 3; ++j) {
+		//映射顶点到视窗
 		float t = -r[j]->z / e.z;
 		float x = e.x + t * r[j]->x;
 		float y = e.y + t * r[j]->y;
+		//将视窗映射到窗口像素
 		p[j].x = int(x * 500);
 		p[j].y = int(y * 500);
 	}
+	//计算包围三角形的最小轴对齐矩形
 	int maxX = max(max(p[0].x, p[1].x), p[2].x);
 	int maxY = max(max(p[0].y, p[1].y), p[2].y);
 	int minX = min(min(p[0].x, p[1].x), p[2].x);
 	int minY = min(min(p[0].y, p[1].y), p[2].y);
+
 	vec3 vec[3] = { vec3(),p[1] - p[0], p[2] - p[0] };
 	for (int i = maxY; i >= minY; --i) {
 		for (int j = minX; j <= maxX; ++j) {
+			//遍历每个像素，计算像素是否在三角形内
 			vec[0] = pos(j, i, 0) - p[0];
 			float M = vec[1].x * vec[2].y - vec[2].x * vec[1].y;
-			float u = (vec[1].x * vec[0].y - vec[0].x * vec[1].y) / M;
-			float v = -(vec[2].x * vec[0].y - vec[0].x * vec[2].y) / M;
-			if (0 <= u && u <= 1 && 0 <= v && v <= 1 - u) {
-				color Color = tri.Acolor * (1 - u - v) + tri.Bcolor * v + tri.Ccolor * u;
+			float v = (vec[1].x * vec[0].y - vec[0].x * vec[1].y) / M;
+			float u = -(vec[2].x * vec[0].y - vec[0].x * vec[2].y) / M;
+			if (0 <= u  && 0 <= v && u+v <= 1) {
+				//如果在，则插值上色
+				color Color = tri.Acolor * (1 - u - v) + tri.Bcolor * u + tri.Ccolor * v;
 				putpixel(j, i, Color.getBGR());
 			}
 		}
